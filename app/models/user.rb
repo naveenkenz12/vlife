6e87c63e4d4a9f32d6585543464ad0e93aa323c9
naveenkenz12 <naveenkenz12@gmail.com>
2016-10-10 00:10:00 +0530

    bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
 	attr_accessor :password
 	EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
 	validates :u_id, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
+	validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
+	validates :password, :confirmation => true
+	validates_length_of :password, :in => 8..20, :on => :create
 end
