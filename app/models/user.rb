a20c49b21c3091d434dfbda30d8bf49abf82ee8c
naveenkenz12 <naveenkenz12@gmail.com>
2016-10-10 00:00:08 +0530

    table users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+	attr_accessor :password
+	EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+	validates :u_id, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
 end
