30cab8f795f5f9995a8ac2e8c7bf835ef1bc68da
Suman <sumanswar12@gmail.com>
2016-10-10 08:04:21 +0530

    Added Sessions:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,12 @@
 class SessionsController < ApplicationController
 	def create
-		
+		user=User.find_by(u_id: params[:session][:u_id])
+		if user && user.authenticate(params[:session][:password])
+
+		else
+			flash.now[:danger] = 'Invalid UserID/Password'
+			render 'new'
+		end	
 	end
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,5 @@
 module SessionsHelper
+	def log_in(user)
+		session[:u_id] = user.id
+	end
 end
