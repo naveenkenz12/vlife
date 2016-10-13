c8b9f4e94847994a8d4d5f8824a000d8c2b45028
naveenkenz12 <naveenkenz12@gmail.com>
2016-10-13 19:03:13 +0530

    comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+	before_action :logged_in_user , only: [:create, :destroy]
+	
+	def create
+		@new_comment = current_user.own_posts.build(comment_params)
+		@new_comment.p_id = Post.count.to_s(36)
+		@new_comment.posted_by_id = current_user.u_id
+		puts @new_comment.parent_id
+		puts @new_comment.p_id
+		puts @new_comment.posted_by_id
+		if @new_comment.save
+			flash[:notice] = "Commenetd Succesfully"
+			msg = {:status => "ok"}
+			render :json => msg
+		else
+			flash[:notice] = "Error!, try again"
+			msg = {:status => "err"}
+			render :json => msg
+		end
+
+	end
+
+	private
+		def comment_params
+			params.permit(:content, :parent_id)
+		end
+end
