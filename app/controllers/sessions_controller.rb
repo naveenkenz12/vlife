f0477b1d3ff8be0bf5bfb902a752381fc3a88689
naveenkenz12 <naveenkenz12@gmail.com>
2016-11-03 19:09:24 +0530

    unprofiled user redirected to fil profile first

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,12 @@ class SessionsController < ApplicationController
 		if user && user.authenticate(params[:session][:password])
 			log_in user
 			remember user
-			redirect_to '/home'
+			@up = UserProfile.find_by(:u_id => user.u_id)
+			if @up.blank?
+				redirect_to '/'+user.u_id+'/about'
+			else
+				redirect_to '/home'
+			end
 		else
 			flash[:danger] = 'Invalid UserID/Password'
 			redirect_to root_url
