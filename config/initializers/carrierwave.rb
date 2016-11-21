8444fbf3042a0a1a2b3aa97814e2c93f58232ed8
Suman <sumanswar12@gmail.com>
2016-11-22 02:57:49 +0530

    FIxes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,15 +2,15 @@ require 'resolv-replace'
 CarrierWave.configure do |config|
   config.fog_credentials = {
   	  :provider				=> 'AWS',
-      :host           => "192.168.0.113",
-      :port           => 8080,
+      :host           => ENV["ASSET_HOST"],
+      :port           => ENV["ASSET_PORT"].to_i,
       :scheme               => 'http',
       :aws_access_key_id      => "TICN3MI355BLA9FZDYCY",
       :aws_secret_access_key  => "K3ydjJYHjVbRws5PnbVtwsa25tpBDFw_iNWJ6w==",
       
   }
   config.fog_public = true
-  config.asset_host = 'http://192.168.0.113:8080'
+  config.asset_host = "http://"+ENV["ASSET_HOST"]+':'+ ENV["ASSET_PORT"]
   config.fog_use_ssl_for_aws = false
   config.storage             = :fog
 end
