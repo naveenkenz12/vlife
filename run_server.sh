1edb658a5b234b5f42df74c9182b0a696e4bd3f9
naveenkenz12 <naveenkenz12@gmail.com>
2016-11-22 13:02:26 +0530

    final

--- run_server.sh
+++ run_server.sh
@@ -3,8 +3,8 @@
 #cd ../src/vlife/
 echo "\nsetting up server\n"
 export ASSET_HOST="192.168.0.113"
-export ASSET_PORT=8080
-export SECRET="K3ydjJYHjVbRws5PnbVtwsa25tpBDFw_iNWJ6w=="
-export KEY="TICN3MI355BLA9FZDYCY"
+export ASSET_PORT="8080"
+export ASSET_SECRET="K3ydjJYHjVbRws5PnbVtwsa25tpBDFw_iNWJ6w=="
+export ASSET_KEY="TICN3MI355BLA9FZDYCY"
 echo "\nrunning server on 0.0.0.0:3000"
-
+rails server -b 0.0.0.0
