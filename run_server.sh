705b2208544dc6b3338cd6a10c48445e3c6c39bf
yak1996 <yathansh.kathuria@gmail.com>
2016-11-22 17:51:11 +0530

    final push mera

--- run_server.sh
+++ run_server.sh
@@ -2,9 +2,9 @@
 
 #cd ../src/vlife/
 echo "\nsetting up server\n"
-export ASSET_HOST="127.0.0.1"
+export ASSET_HOST="192.168.0.113"
 export ASSET_PORT=8080
 export SECRET="K3ydjJYHjVbRws5PnbVtwsa25tpBDFw_iNWJ6w=="
 export KEY="TICN3MI355BLA9FZDYCY"
 echo "\nrunning server on 0.0.0.0:3000"
-rails server -b 0.0.0.0
+
